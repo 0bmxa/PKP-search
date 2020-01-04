8d0deeb2736d7146390b4f8ee73deb63cbb832a7
0bmxa <0bmxa@users.noreply.github.com>
2020-01-04 20:44:35 +0100

    Button loading title; Berlin Hbf

--- app.js
+++ app.js
@@ -6,13 +6,16 @@
 function App() {
     const intercitySearchURL = 'https://www.intercity.pl/en/site/for-passengers/information/journey-planner.html';
     const stations = [
+        // Find more station IDs here:
+        // https://rozklad-pkp.pl/station/search?term=[STATION-NAME]
         // {name: 'Berlin (all)',       id: '8096003'},
         // {name: 'Warszawa (all)',     id: '5196003'},
-        // {name: 'Krakow (all)',       id: '5196001'},        
-        {name: 'Berlin Ost',         id: '8010255'},
-        {name: 'Warszawa Zachodnia', id: '5100067'},
-        {name: 'Warszawa Centralna', id: '5100065'},
-        {name: 'Krakow Główny',      id: '5100028'},
+        // {name: 'Krakow (all)',       id: '5196001'},
+        {name: 'Berlin Hauptbahnhof', id: '8011160'},
+        {name: 'Berlin Ost',          id: '8010255'},
+        {name: 'Warszawa Centralna',  id: '5100065'},
+        {name: 'Warszawa Zachodnia',  id: '5100067'},
+        {name: 'Krakow Główny',       id: '5100028'},
     ];
 
 
@@ -26,13 +29,18 @@ function App() {
         $('#intercity-frame')[0].src = intercitySearchURL + searchString;
     }
 
+    const setButtonLoading = function(loading) {
+        const buttonTitle = (loading == true) ? 'Loading…' : 'Search';
+        $('#search').text(buttonTitle);
+    }
+
     const updateUIFromURLParams = function() {
         const urlParams = new URLSearchParams(window.location.search);
 
         const start = urlParams.get('stid[0]');
-        const end = urlParams.get('stid[1]');
-        const date = urlParams.get('date');
-        const time = urlParams.get('time');
+        const end   = urlParams.get('stid[1]');
+        const date  = urlParams.get('date');
+        const time  = urlParams.get('time');
 
         // min date
         const now = new Date();
@@ -42,8 +50,9 @@ function App() {
         const tomorrow = new Date(now.getTime() + 24*3600*1000);
         const tomorrowISO = tomorrow.toISOString().substr(0,10);
 
+        // Set UI from URL params, or use default values
         $('#start').val(start || stations[0].id);
-        $('#end')  .val(end   || stations[1].id);
+        $('#end')  .val(end   || stations[2].id);
         $('#date') .val(date  || tomorrowISO)
         $('#time') .val(time  || '12:00');
     }
@@ -63,7 +72,12 @@ function App() {
         urlParams.set('search', '1');
 
         const searchString = '?' + urlParams.toString();
+        setButtonLoading(true);
         loadFrameWithParams(searchString);
+    })
+
+    $('#intercity-frame').on('load', function(e) {
+        setButtonLoading(false);
     })
 }
 App();
